6f0df553062051c393858b1a6bcb9b57e9d25f56
redward <rajive@gspace.wlc.ac.uk>
2015-01-22 09:59:54 +0000

    Structure updates

--- index.php
+++ index.php
@@ -28,15 +28,15 @@
   		</div><!-- /.container-fluid -->
 	</nav>
 	<div class="container">
-		<div class="col-md-6">
-			<h1 id="main-heading">Tube</h1>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-			<h2>And then...</h2>
-			<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-		</div>
-		<div class="col-md-6">
-			<h2>DLR</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		<div class="col-md-12">
+			<ul id="stations">
+				<li>KSX</li>
+				<li>LHR</li>
+				<li>DXB</li>
+				<li>BKE</li>
+				<li>CHX</li>
+				<li>LAX</li>
+			</ul>
 		</div>
 	</div>
 	<script src="/js/jquery-2.1.3.min.js"></script>
