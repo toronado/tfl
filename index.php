d45de56e1e00fe03f856a6b77e1b861053459d78
redward <rajive@gspace.wlc.ac.uk>
2015-01-26 16:58:48 +0000

    JS working

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
+<?php
+$mysqli = new mysqli("localhost", "toro", "nado", "tfl");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} else {
+	$result = $mysqli->query("SELECT id FROM stations LIMIT 10");
+	printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+?>
 <head>
 	<title>TFL Train Times</title>
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
@@ -16,11 +28,19 @@
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      	<ul class="nav navbar-nav">
 		        	<li class="dropdown">
-		          		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		          		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Lines <span class="caret"></span></a>
 		          		<ul id="main-menu" class="dropdown-menu" role="menu">
-		            		<li><a href="#one">Action</a></li>
-		            		<li><a href="#two">Another action</a></li>
-		            		<li><a href="#three">Something else here</a></li>
+		            		<li><a href="#bakerloo">bakerloo</a></li>
+		            		<li><a href="#central">central</a></li>
+		            		<li><a href="#circle">circle</a></li>
+		            		<li><a href="#district">district</a></li>
+		            		<li><a href="#hammersmith-city">hammersmith-city</a></li>
+		            		<li><a href="#jubilee">jubilee</a></li>
+		            		<li><a href="#metropolitan">metropolitan</a></li>
+		            		<li><a href="#northern">northern</a></li>
+		            		<li><a href="#piccadilly">piccadilly</a></li>
+		            		<li><a href="#victoria">victoria</a></li>
+		            		<li><a href="#waterloo-city">waterloo-city</a></li>
 		          		</ul>
 		        	</li>
 		      	</ul>
@@ -29,10 +49,7 @@
 	</nav>
 	<div class="container">
 		<div class="col-md-12">
-			<div id="stations">
-				results
-				<p id="button">click</p>
-			</div>
+			<div id="stations"></div>
 		</div>
 	</div>
 	<script src="/js/jquery-2.1.3.min.js"></script>
--- js/tfl.js
+++ js/tfl.js
@@ -1,39 +1,42 @@
-var mainHeading = document.getElementById('main-heading');
 $('#main-menu').on('click', 'a', function() {
-	var href = $(this).attr('href');
-	mainHeading.innerHTML = href + ' selected!';
+	var line = $(this).attr('href').split('#')[1];
+	getStns(line);
 	return false;
 });
 
-$('#button').on('click', function() {
-	getStns();
-});
-
 function getStns(line) {
+
 	$.ajax({
-        url: 'resources/station-data/central.js',
+        url: 'resources/station-data/'+line+'.js',
         dataType: 'json',
         timeout: 30000
     })
     .done(function (data){
     	var $results = $('#stations');
+    	var stations = '';
 		var i;
 		var d = data.length;
 		var count = 0;
 
 		for (i=0; i<d; i++) {
 			for (var prop in data[i]) {
-            	if (prop == 'naptanId') {
+            	if (prop == '$type') {
             		var naptanId = data[i]['naptanId'].substring(8,11);
             		var name = data[i]['commonName'].replace(' Underground Station', '');
-            		var lines = data[i]['lineGroup'];
-            		$results.append('<p value="'+naptanId+'">'+name+ ' - ' + naptanId + ' - ' + lines[lines.length-1].lineIdentifier + '</p>');
+            		var lineType = data[i]['lineModeGroups'].length;
+            		var lines = '';
+            		for (var j=0; j<lineType; j++) {
+            			if (data[i]['lineModeGroups'][j].modeName === 'tube') {
+            				lines = data[i]['lineModeGroups'][j].lineIdentifier;
+            				break;
+            			}
+            		}
+            		stations += '<li value="'+naptanId+'">' + naptanId + ' - ' + name + ' - ' + lines + '</li>';
+            		count+=1;
             		break;
             	}
-            	count+=1;
         	}
 		}
-		$results.prepend('<h2>'+count+' stations</h2>');
-		console.log(count);
+		$results.html('<h2>'+line+' line - '+count+' stations</h2>' + '<ul class="'+line+'">' + stations + '</ul>');
     });
 }
