4d8465f497cbccfec322f13403575700d43e1f88
rajive <rajinhoeduardo@gmail.com>
2015-05-04 23:47:51 +0100

    nm

--- templates/arrivals.php
+++ templates/arrivals.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-fixed-top fadeInDown animated" id="nav-arrivals">
   	<div class="container-fluid">
-    	<h2 class="navbar-text pull-left" style="width:75%;">
+    	<h2 class="navbar-text pull-left" style="width:60%;">
     		<a href="/tfl/#/" style="color:#fff;">{{station.name}}</a>
     		<small>
 				{{filtered.length}} arrivals
@@ -18,7 +18,7 @@
 				</span>
 			</small>
     	</h2>
-    	<h2 class="navbar-text pull-right" style="text-transform:lowercase; width:25%; text-align:right;">
+    	<h2 class="navbar-text pull-right" style="text-transform:lowercase; width:40%; text-align:right;">
 			<a href="" id="counter" class="status-{{liveStatus}}" data-ng-click="switchLive(1-liveStatus)">
 				{{timestamp | date:'h:mma'}}
 				<sup><span data-ng-bind="count"><i class="fa fa-cog fa-spin"></i></span></sup>
@@ -30,35 +30,33 @@
 	<div id="map-canvas"></div>
 </div>
 <div class="container-fluid" id="arrivals">
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="lines nav nav-pills nav-justified">
-				<li class="line" data-ng-repeat="(line,platforms) in station.lines">
-					<a class="bg-{{line | stnName}}" href="" data-ng-click="filters.setFilters(line,'')">
-						{{(arrivals|filter:{lineName:line}).length}} {{line.substr(0,3)}}
-					</a>
-					<ul class="platforms nav nav-pills nav-justified">
-						<li class="platform" data-ng-repeat="platform in platforms">
-							<a href="" data-ng-click="filters.setFilters(line,platform)">
-								<i class="fa {{platform | arrowDirection}} {{line | stnName}}"></i>
-								<span>{{(arrivals|filter:{lineName:line, platformName:platform}).length}}</span>
-							</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-			<ul id="arrival-list">
-				<li class="arrival fadeInUp animated" data-ng-repeat="arrival in filtered = (arrivals | orderBy:'-timeToStation':true | filter:filters )">
-					<span class="badge">
-						{{arrival.timeToStation | convertTime}}
-					</span>
-					<i class="{{arrival.lineId}} fa {{arrival.platformName | arrowDirection}}"></i>
-					{{arrival.towards}}
-					<small>
-						{{arrival.platformName.split(' ')[0]}}, {{arrival.currentLocation}}
-					</small>
-				</li>
-			</ul>
-		</div>
+	<div class="col-md-12" style="padding:0;">
+		<ul class="lines nav nav-pills nav-justified">
+			<li class="line" data-ng-repeat="(line,platforms) in station.lines">
+				<a class="bg-{{line | stnName}}" href="" data-ng-click="filters.setFilters(line,'')">
+					{{(arrivals|filter:{lineName:line}).length}} {{line.substr(0,3)}}
+				</a>
+				<ul class="platforms nav nav-pills nav-justified">
+					<li class="platform" data-ng-repeat="platform in platforms">
+						<a href="" data-ng-click="filters.setFilters(line,platform)">
+							<i class="fa {{platform | arrowDirection}} {{line | stnName}}"></i>
+							<span>{{(arrivals|filter:{lineName:line, platformName:platform}).length}}</span>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<ul id="arrival-list">
+			<li class="arrival fadeInUp animated" data-ng-repeat="arrival in filtered = (arrivals | orderBy:'-timeToStation':true | filter:filters )">
+				<span class="badge">
+					{{arrival.timeToStation | convertTime}}
+				</span>
+				<i class="{{arrival.lineId}} fa {{arrival.platformName | arrowDirection}}"></i>
+				{{arrival.towards}}
+				<small>
+					{{arrival.platformName.split(' ')[0]}}, {{arrival.currentLocation}}
+				</small>
+			</li>
+		</ul>
 	</div>
 </div>
--- templates/stations.php
+++ templates/stations.php
@@ -1,12 +1,8 @@
 <nav class="navbar navbar-fixed-top" id="nav-stations">
-    <input id="station-search" type="text" data-ng-model="search" placeholder="station search">
+    <input id="station-search" type="text" data-ng-model="search" placeholder="search">
 </nav>
 <div class="container-fluid" id="stations">
-	<div class="row">
-		<div class="col-md-12">
-			<ul id="station-list" class="list-unstyled fadeIn animated" filter-list="search">
-				<?php include 'station-list.html'; ?>
-			</ul>
-		</div>
-	</div>
+	<ul id="station-list" class="list-unstyled fadeIn animated" filter-list="search">
+		<?php include 'station-list.html'; ?>
+	</ul>
 </div>
